992833e1ee99e6339233c9cf47d3c0d509b962ec
Gerard Giné <gerard.gine@me.com>
2019-12-30 02:03:06 -0800

    Initial Commit

---
+++ swift-store-checker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-store-checker
+//
+//  Created by Gerard Giné on 12/30/19.
+//  Copyright © 2019 Gerard Giné. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
